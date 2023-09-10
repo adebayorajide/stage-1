0879cc0432afdc2ea9c35736efbe4f87e86f0642
Ajide Tosin Adebayo <47295073+adebayorajide@users.noreply.github.com>
2023-09-10 23:35:51 +0100

    first commit
    A webpage displaying slack username, profile picture and stack

---
+++ index.js
@@ -0,0 +1,23 @@
+const slackName = document.querySelector(".slack_name");
+const imageEl = document.querySelector(".image");
+const currentDay = document.querySelector(".current_day");
+const currentTime = document.querySelector(".time");
+const trackEl = document.querySelector(".track");
+
+const profile = {
+    name: "Adebayorajide",
+    img: "https://ca.slack-edge.com/T05FFAA91JP-U05S0AU36L8-b655037ef848-512",
+    track: "Frontend Development",
+};
+
+let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const currentDate = new Date();
+let day = weekday[currentDate.getDay()];
+
+window.addEventListener("load", ()=>{
+    slackName.innerHTML = profile.name;
+    imageEl.src = profile.img;
+    trackEl.innerHTML = profile.track;
+    currentDay.innerHTML = day;
+    currentTime.innerHTML = currentDate.getUTCMilliseconds();
+})
